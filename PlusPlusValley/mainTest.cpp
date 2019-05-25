75a809092fc1a4aec3ae3a8a5393a8fc31b654f9
Sunmon <blackmilk274@gmail.com>
2019-05-25 13:32:20 +0900

    Add testing code

--- PlusPlusValley/mainTest.cpp
+++ PlusPlusValley/mainTest.cpp
@@ -1,7 +1,15 @@
 // testing git solution
+// 테스트용 코드
 #include <iostream>
+#include "character.h"
+
 int main()
 {
 	std::cout << "hi" << std::endl;
+
+	//test for character
+	character* testChar = new character();
+	testChar->setx(10);
+	std::cout << testChar->getx() << std::endl;
 
 }
