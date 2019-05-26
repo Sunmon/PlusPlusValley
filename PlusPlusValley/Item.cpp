cceb096ba8d3d3005ae4959f60238d50727081e7
Sunmon <blackmilk274@gmail.com>
2019-05-26 19:48:56 +0900

    Add Item Class
    Item 클래스 추가. 기본적인 변수 생성

--- PlusPlusValley/Item.cpp
+++ PlusPlusValley/Item.cpp
@@ -1,12 +1,4 @@
 #include "Item.h"
-/**
-* Item을 나타내기 위한 클래스
-* 사용 방법: 
-* 
-* @Author Sunmon
-* @Date 2019.05.26
-*/
-
 
 Item::Item()
 {
--- PlusPlusValley/Item.h
+++ PlusPlusValley/Item.h
@@ -1,12 +1,19 @@
 #pragma once
+/**
+* Item을 관리하는 클래스
+* 사용 방법:
+* @author KIM SUN JUNG
+* @date 2019.05.26
+*/
 class Item
 {
 public:
 	Item();
+	Item(enum itemType);
 	~Item();
 
 private:
-	enum itemType;
-
+	enum itemType {SEED, CROP, STONE, WOOD, TOOL};	//아이템 타입. 씨앗, 수확작물, 돌, 나무, 도구
+	const int MAX_NUM_OF_ITEM;	//아이템별 가질 수 있는 최대 수
+	int cost;	//가격
 };
-
